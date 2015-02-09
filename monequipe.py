10e3f379e4bc6bef828883a70745635ff9974495
SebastienBussonnais <3300200@ari-41-311-07.infop6.jussieu.fr>
2015-02-09 18:52:04 +0100

    MAJ ENORME

--- monequipe.py
+++ monequipe.py
@@ -4,23 +4,23 @@ from soccersimulator import pyglet
 from strats import AllerVersBallon, Tir, VideS, AllerVersLoc, CompoStrat, Fonceur, AllerButBallon, Degagement, Defenseur, Dribble, Dribbleur, Interception, Intercepteur
 
 team1=SoccerTeam("Pandragon 1v1")
-team8.add_player(SoccerPlayer("t1j1",Intercepteur()))
+team1.add_player(SoccerPlayer("t1j1",Intercepteur()))
 
 team2=SoccerTeam("Super Pandragon 1v1")
-team9.add_player(SoccerPlayer("t1j1",Intercepteur()))
+team2.add_player(SoccerPlayer("t1j1",Intercepteur()))
 
 team3=SoccerTeam("Tueur de fonceur 2v2")
-team2.add_player(SoccerPlayer("t1j1",Intercepteur()))
-team2.add_player(SoccerPlayer("t1j2",Defenseur()))
+team3.add_player(SoccerPlayer("t1j1",Intercepteur()))
+team3.add_player(SoccerPlayer("t1j2",Defenseur()))
 
 team4=SoccerTeam("Versus Trying")
 team4.add_player(SoccerPlayer("t1j1",Fonceur()))
 team4.add_player(SoccerPlayer("t1j2",Dribbleur()))
 
 team5=SoccerTeam("Unicorn of Love 4v4")
-team3.add_player(SoccerPlayer("t1j1",Defenseur()))
-team3.add_player(SoccerPlayer("t1j2",Fonceur()))
-team3.add_player(SoccerPlayer("t1j3",Dribbleur()))
-team3.add_player(SoccerPlayer("t1j4",Intercepteur()))
+team5.add_player(SoccerPlayer("t1j1",Defenseur()))
+team5.add_player(SoccerPlayer("t1j2",Fonceur()))
+team5.add_player(SoccerPlayer("t1j3",Dribbleur()))
+team5.add_player(SoccerPlayer("t1j4",Intercepteur()))
 
 teams =[team1,team2, team3, team4, team5, team6]
